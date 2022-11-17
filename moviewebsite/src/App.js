e527d6eb5d5ddcb9c80b46920ea3f165b0d21e6d
Jose Cruz <jose.cruz@kununu.com>
2022-11-17 11:21:55 +0000

    Added Search files - not implemented

--- moviewebsite/src/App.js
+++ moviewebsite/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //Importing containers/components
 //import Home from './Containers/Home/Home';
 import Showcase from './Containers/Showcase/Showcase';
+import Search from './Containers/Search/Search';
 
 function App() {
   return (
@@ -14,7 +15,9 @@ function App() {
       
         <Routes>
 
-          <Route path="/" element={<Showcase/>}/>
+          <Route path="/" element={<Showcase/>}/>;
+          {/*<Search name={'Jim'}/>;*/}
+
         </Routes>
       
       </BrowserRouter>
